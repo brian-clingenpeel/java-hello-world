48b7b91b441ff29c0c710ac07a80277242b76474
sly <sly@localhost.localdomain>
2025-09-01 19:01:09 -0500

    updated helloword x+y

--- helloworld.java
+++ helloworld.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+        int x = 5
+        int y = 4
+        int xplusy = x + y
+        System.out.pritnln(xplusy);
+    
 }
